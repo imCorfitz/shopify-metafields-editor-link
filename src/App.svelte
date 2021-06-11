fb2bba1087b7482e298552e609a946b053aee8c5
Corfitz <hi@imcorfitz.com>
2021-06-11 12:41:05 +0100

    initial commit v.1.0.0

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,169 @@
 <script>
-  import { onMount } from "svelte";
-  export let date;
+  import {
+    Content,
+    DataTable,
+    Button,
+    Grid,
+    Row,
+    Column,
+    Tabs,
+    TabContent,
+    Tab,
+    CodeSnippet,
+    Select,SelectItem,
+    TextInput,
+    Form,FormGroup,
+  } from "carbon-components-svelte";
+  import TrashCan16 from "carbon-icons-svelte/lib/TrashCan16";
+  import Header from "./components/Header.svelte";
+  let url;
+  let site = "";
+  let metafield = "";
+  let resource = "Product";
+  let loading = false;
+  let disabled = true;
+  let invalid = false;
 
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
-  });
+   const headers = [
+    { key: "name", value: "Metafield" },
+    { key: "overflow", empty: true },
+  ];
+
+  let metafields = [{name: "Colour", id: "colour"}, {name: "Designer name", id: "designer_name"}];
+
+  function generateMetafields() {
+    const d = metafields.map(e => `metafields.global.${e.id}`);
+    return d.join('%2C');
+  }
+
+  function removeMetafield(row) {
+      metafields = metafields.filter(m => m.id !== row.id);
+  }
+
+  function addMetafield() {
+      const RegExpression = /^[a-zA-Z\s]*$/;
+
+      if (!metafield || !RegExpression.test(metafield)) {
+          invalid = true;
+          return;
+      }
+
+      const name = metafield.trim();
+      const id = name.toLowerCase().replace(/\s/g, "_");
+
+       if (metafields.find(e => e.id === id)) {
+          invalid = true;
+          return;
+      }
+
+      metafields = [...metafields, {name, id}]
+
+      metafield = "";
+
+  }
+$: {
+    if (site && metafields) {
+        url = `${site.replace(/\/$/, "")}/admin/bulk?resource_name=${resource}&edit=${generateMetafields()}`;
+        disabled = false;
+    } else {
+        url = "First you need to add site and metafields..."
+        disabled = true;
+    }
+};
+ $: document.documentElement.setAttribute("theme", "g10");
 </script>
 
-<main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/main/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
-</main>
+  <Header />
+  <Content style="background: none; padding: 1rem">
+    <Grid>
+      <Row style="padding-top: 2rem;">
+        <Column lg="{16}">
+          <h1 style="margin-bottom: 1.5rem">Generate Shopify Metafields Editor Link</h1>
+          <p style="margin-bottom: 1.5rem">Generate the link needed to access custom metafields for products in your Shopify Store without the need for a plugin or app.</p>
+          <h5 style="margin-bottom: 1rem">Copy the admin URL</h5>
+                      <CodeSnippet size="xl"  code={url} skeleton={loading} {disabled} hideCopyButton={disabled} />
+        </Column>
+      </Row>
+
+      <Row style="margin-top: 1rem">
+        <Column noGutter>
+          <Tabs aria-label="Tab navigation">
+            <Tab label="Generator" />
+            <Tab label="Why" />
+            <Tab label="How" />
+            <div slot="content" class="tabbed-content">
+              <Grid as fullWidth let:props>
+                <TabContent {...props}>
+                  <Row>
+                    <Column md={5} >
+
+                        <TextInput bind:value={site}
+                       size="xl" required invalidText="This field is requried" labelText="Your shopify URL" placeholder="Shopify URL"
+  helperText="This can be your site's domain name or your actual myshopify URL. E.g. 'https://my-coffee-store.myshopify.com' or 'https://coffestore.com'"/>
+
+   <Select  style="margin-top: 1rem"
+                        labelText="Resource" size="xl"
+                        bind:selected="{resource}"
+                      >
+                        <SelectItem value="Product" text="Product" />
+                        <SelectItem value="Collection" text="Collection" />
+                      </Select>
+
+                      <h5 style="margin-top: 1rem">Metafields</h5>
+                      <p>Add here the custom meta fields you wish to be able to manage.</p>
+                      {#if metafields}
+                          <DataTable  {headers} rows={metafields}>
+                            <span slot="cell" let:cell let:row>
+                                {#if cell.key === 'overflow'}
+                                <Button kind="danger" iconDescription="Remove" on:click={() => removeMetafield(row)} icon={TrashCan16} />
+                                {:else}{cell.value}{/if}
+                            </span>
+                            </DataTable>
+                      {/if}
+                      <Form style="margin-top: 1rem" on:submit={addMetafield}>
+
+                      <h5 style="margin-top: 1rem">Add new metafield</h5>
+                             <TextInput bind:value={metafield}
+                       size="xl" on:change={() => {invalid = false}} {invalid} invalidText="This field is requried and should be unique. Keep it simple. Letters and spaces only." labelText="Field name" placeholder="e.g. Colour, Designer name"
+  helperText="Keep it simple. Letters and spaces only."/>
+  <Button  style="margin-top: 1rem" type="submit">Add Metafield</Button>
+
+                      </Form>
+
+
+                    </Column>
+                  </Row>
+                </TabContent>
+                <TabContent {...props}>
+                  <Row>
+                    <Column md={5} >
+                      <p>
+                        Was watching "Code with Chris" and his video on <a href="https://www.youtube.com/watch?v=UbwhADWZzvQ" target="_blank">Custom Fields in Shopify</a>, and thought it would be good to have such tool available to simplify access to metafields in the Shopify Admin.
+                      </p>
+                    </Column>
+                  </Row>
+                </TabContent>
+                <TabContent {...props}>
+                  <Row>
+                    <Column md={5} >
+                        <p style="margin-bottom: 1rem">
+                        After you have added your custom metafields, you can simply include them in your theme code in liquid as following:
+
+                      </p>
+                      <CodeSnippet  style="margin-bottom: 1rem" code={"{{ product.metafields.global.field_name }}"}   />
+                      <p style="margin-bottom: 1rem">
+                        This solution has a few limitations, as you are limited to only using global metafields, and you can't nest metafield objects.
+                      </p>
+                      <p style="margin-bottom: 1rem">
+                        This tool is public repo available at <a href="https://github.com/imCorfitz/shopify-metafields-editor-link" target="_blank">https://github.com/imCorfitz/shopify-metafields-editor-link</a>
+                      </p>
+                    </Column>
+                  </Row>
+                </TabContent>
+              </Grid>
+            </div>
+          </Tabs>
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+  import {
+    SkipToContent,
+    Header,
+  } from "carbon-components-svelte";
+</script>
+
+<Header company="Shopify" platformName="Generate Metafields Editor Link" href="/">
+  <div slot="skip-to-content">
+    <SkipToContent />
+  </div>
+</Header>
